af29d2fa48d5aadda9eff37a2e52870a0e3fee86
DeadManBP <incarnatedeath666@gmail.com>
2026-08-14 15:22:44 -0400

    Bump APK version for verified Power & Utilities build

--- BrockvilleTestHub/app/build.gradle.kts
+++ BrockvilleTestHub/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {compileOptions {
         applicationId="ca.brockvillehub.test"
         minSdk=23
         targetSdk=35
-        versionCode=1
-        versionName="0.1.0"
+        versionCode=2
+        versionName="0.1.1"
     }
 }
 dependencies {
