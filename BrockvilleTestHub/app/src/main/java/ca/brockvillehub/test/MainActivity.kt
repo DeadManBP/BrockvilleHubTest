fe268cfbbd46de1041d278f4878130c0efe46f1a
DeadManBP <incarnatedeath666@gmail.com>
2026-08-14 06:05:32 -0400

    Fix Android back button navigation

--- BrockvilleTestHub/app/src/main/java/ca/brockvillehub/test/MainActivity.kt
+++ BrockvilleTestHub/app/src/main/java/ca/brockvillehub/test/MainActivity.kt
@@ -1,21 +1,37 @@
 package ca.brockvillehub.test
+
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity() {
- @SuppressLint("SetJavaScriptEnabled")
- override fun onCreate(savedInstanceState: Bundle?) {
-  super.onCreate(savedInstanceState)
-  val w=WebView(this)
-  w.settings.javaScriptEnabled=true
-  w.settings.domStorageEnabled=true
-  w.webViewClient=WebViewClient()
-  w.webChromeClient=WebChromeClient()
-  setContentView(w)
-  w.loadUrl("file:///android_asset/index.html")
- }
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        setContentView(webView)
+        webView.loadUrl("file:///android_asset/index.html")
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+    }
 }
